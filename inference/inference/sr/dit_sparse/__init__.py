d36c0fda2774a33aef5caa7c5f87a60cf0a3acab
xin1u <luxion@mail.ustc.edu.cn>
2026-06-04 12:48:53 +0800

    Remove duplicate nested inference directory

--- inference/inference/sr/dit_sparse/__init__.py
+++
@@ -1,4 +0,0 @@
-from .models import Transformer3DModel, WanModel, sinusoidal_embedding_1d, WanAttentionBlock
-
-
-__all__ = ["Transformer3DModel", "WanModel", "sinusoidal_embedding_1d", "WanAttentionBlock"]
--- inference/inference/sr/latent_upsampler/__init__.py
+++
@@ -1,5 +0,0 @@
-from .ultra_latent_up_v1 import UltraLatentUpsampler
-from .ultra_latent_up_v2 import UltraLatentUpV2
-from .ultra_latent_up_v3 import UltraLatentUpV3
-
-__all__ = ["UltraLatentUpsampler", "UltraLatentUpV2", "UltraLatentUpV3"]
